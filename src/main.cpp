c8fdd42013afcb251d2e9556a5b82f99d18c329e
Fredrik Hultin <fredrik.hultin@netclean.com>
2015-11-27 09:39:05 +0100

    Disabled fix for msvcrt abort window, since it causes the application to crash on launch.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 {
 	SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
 	// Suppress the abort message
-	_set_abort_behavior(0, _WRITE_ABORT_MSG);
+	//_set_abort_behavior(0, _WRITE_ABORT_MSG);
 
 	return Program::Create()->Run(argc, argv);
 }
