aaff9592ac5211d7f80051c4d7622956f0523780
Fredrik Hultin <fredrik.hultin@netclean.com>
2016-01-27 16:04:05 +0100

    added docker instructions

---
+++ docker/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+BUILD_ROOT=/root/build
+
+SPANK_REPO=https://github.com/noname22/spank.git
+SPANK=$BUILD_ROOT/spank/spank
+
+ARTIFACTS="SSG.VideoPlayer.exe"
+
+set -e
+
+# set up build directory
+mkdir -p $BUILD_ROOT
+
+# clone and build spank
+git clone $SPANK_REPO $BUILD_ROOT/spank
+cd $BUILD_ROOT/spank
+./build.sh
+
+# make a copy of the source tree and build there
+# because strip fails in docker on windows (virtualbox shared folders)
+# https://www.virtualbox.org/ticket/8463
+
+cd $BUILD_ROOT
+cp -r source source-copy
+cd source-copy
+
+# build dependencies
+cd $BUILD_ROOT/source-copy/deps
+./deps.sh all 32bit
+
+# build release
+cd $BUILD_ROOT/source-copy
+$SPANK --jobs 4 rebuild release
+
+# copy back the artifacts to the mounted directory
+cp $ARTIFACTS ../source
