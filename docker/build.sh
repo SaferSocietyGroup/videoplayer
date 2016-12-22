3e24102c7a70e4c799efc0629713e8990d3ee2af
Fredrik Hultin <fredrik.hultin@netclean.com>
2016-12-22 16:53:20 +0100

    Updated docker file and moved deps submodule and bumped version.

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,6 @@
 BUILD_ROOT=/root/build
 
 SPANK_REPO=https://github.com/noname22/spank.git
-SPANK=$BUILD_ROOT/spank/spank
 
 ARTIFACTS="SSG.VideoPlayer.exe"
 
@@ -19,18 +18,9 @@ cd $BUILD_ROOT
 cp -r source source-copy
 cd source-copy
 
-# clone and build spank
-git clone $SPANK_REPO $BUILD_ROOT/spank
-cd $BUILD_ROOT/spank
-./build.sh
-
-# build dependencies
-cd $BUILD_ROOT/source-copy/deps
-./deps.sh all 32bit
-
 # build release
 cd $BUILD_ROOT/source-copy
-$SPANK --jobs 4 rebuild release
+spank --jobs 4 rebuild release
 
 # copy back the artifacts to the mounted directory
 cp $ARTIFACTS ../source
